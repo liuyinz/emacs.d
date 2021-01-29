de2fb9404b4a18bd23f2c9591bd56e34ae907c56
liuyinz <cheunghsu@outlook.com>
2021-01-29 20:57:35 +0800

    feat : switch to selectrum

--- core/init-selectrum.el
+++ core/init-selectrum.el
@@ -2,49 +2,72 @@
 ;;; Commentary:
 ;;; Code:
 
-(use-package selectrum
-  :blackout (selectrum-mode)
-  :hook (after-init . selectrum-mode)
-  :init
-  (setq selectrum-extend-current-candidate-highlight t
-        selectrum-count-style 'current/matches)
-  :bind (
-         ("C-x C-z" . selectrum-repeat)
-         ))
+(leaf orderless
+  :require t
+  :init (setq completion-styles '(orderless)))
 
-(use-package selectrum-prescient
-  ;; :disabled
+(leaf selectrum
+  :blackout t
+  :hook (after-init-hook . selectrum-mode)
+  :bind
+  ("C-x C-z" . selectrum-repeat)
+  (:selectrum-minibuffer-map
+   ([escape] . minibuffer-keyboard-quit))
+  :init
+  (setq selectrum-max-window-height 15
+        selectrum-extend-current-candidate-highlight t
+        selectrum-count-style 'current/matches
+        selectrum-complete-in-buffer nil)
+  :config
+  (with-eval-after-load 'orderless
+    (setq selectrum-refine-candidates-function #'orderless-filter)
+    (setq selectrum-highlight-candidates-function #'orderless-highlight-matches)))
+
+(leaf selectrum-prescient
+  :require t
   :blackout (selectrum-prescient-mode prescient-persist-mode)
-  :hook (selectrum-mode . (lambda()
-                            (selectrum-prescient-mode)
-                            (prescient-persist-mode)))
-  :init
-  (setq prescient-history-length 300))
+  :hook (selectrum-mode-hook . (lambda()
+                                 (selectrum-prescient-mode)
+                                 (prescient-persist-mode)))
+  :init (setq prescient-history-length 300))
 
-
-(use-package embark
-  :bind(
-        :map selectrum-minibuffer-map
-        ("C-c C-o" . embark-export)
-        ("C-c C-c" . embark-act)))
-
-(use-package consult
-  :bind(
-        ( [remap switch-to-buffer] . consult-buffer)
-        ( [remap switch-to-buffer-other-window] . consult-buffer-other-window)
-        ( [remap switch-to-buffer-other-frame] . consult-buffer-other-frame)
-        ))
+(leaf consult
+  :bind
+  ( [remap switch-to-buffer] . consult-buffer)
+  ( [remap switch-to-buffer-other-window] . consult-buffer-other-window)
+  ( [remap switch-to-buffer-other-frame] . consult-buffer-other-frame))
 ;; (when (executable-find "rg")
 ;;   (global-set-key (kbd "M-?") 'consult-ripgrep))
 
-(use-package embark-consult)
-
-(use-package consult-flycheck)
-
-(use-package marginalia
-  :hook (after-init . (lambda () (marginalia-mode 1)))
+(leaf embark
+  :require t
+  :after selectrum
+  :bind(:selectrum-minibuffer-map
+        ("C-c C-o" . embark-export)
+        ("C-c C-c" . embark-act))
   :config
-  (setq-default marginalia-annotators '(marginalia-annotators-heavy)))
+  (leaf embark-consult
+    :blackout t
+    :hook (embark-collect-mode-hook . embarkconsult-preview-minor-mode)))
+
+;; (leaf embark-consult
+;;   :blackout t
+;;   :after (embark consult)
+;;   :hook (embark-collect-mode-hook . embarkconsult-preview-minor-mode))
+
+;; (leaf consult-flycheck)
+
+(leaf marginalia
+  :hook (selectrum-mode-hook . marginalia-mode)
+  :bind
+  (:minibuffer-local-map
+   ("C-M-a" . marginalia-cycle))
+  (:embark-general-map
+   ("A" . marginalia-cycle))
+  :config
+  (setq-default marginalia-annotators '(marginalia-annotators-heavy))
+  (advice-add #'marginalia-cycle :after
+              (lambda () (when (bound-and-true-p selectrum-mode) (selectrum-exhibit)))))
 
 
 (provide 'init-selectrum)
