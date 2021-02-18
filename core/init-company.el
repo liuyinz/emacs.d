8889dd6a7c196e076c68d1ecb5d091760d1bea37
liuyinz <cheunghsu@outlook.com>
2021-02-18 19:59:19 +0800

    fix : separate company and yasnippet settings

--- core/init-company.el
+++ core/init-company.el
@@ -11,26 +11,22 @@
   :commands company-cancel
   :hook (after-init-hook . global-company-mode)
   :bind
-  (:company-mode-map
-   ("C-;" . company-yasnippet))
   (:company-active-map
-   ("C-;" . my-company-yasnippet)
    ([escape] . company-abort)
-   ([tab] . company-complete-common-or-cycle)
-   ((kbd "<backtab>") . company-select-previous)
+   ([tab] . company-yas)
    ("M-n" . nil)
    ("M-p" . nil)
-   ("C-n" . company-select-next)
+   ("C-n" . company-complete-common-or-cycle)
    ("C-p" . company-select-previous)
    ("C-/" . company-filter-candidates))
   (:company-search-map
-   ([tab] . company-select-next)
-   ((kbd "<backtab>") . company-select-previous)
+   ;; ([tab] . company-select-next)
+   ;; ((kbd "<backtab>") . company-select-previous)
    ("C-n" . company-select-next)
    ("C-p" . company-select-previous))
   :init
-  (setq tab-always-indent 'complete
-        company-tooltip-width-grow-only t
+  (setq company-tooltip-width-grow-only t
+        company-minimum-prefix-length 1
         company-idle-delay 0
         company-require-match nil
         company-selection-wrap-around t
@@ -54,65 +50,19 @@
                            company-files
                            company-dabbrev))
 
-  ;; @https://emacs-china.org/t/company-yasnippet/12232/13?u=cheunghsu
-  (defun my-company-yasnippet ()
-    "Hide the current completeions and show snippets."
+  (defun company-yas ()
+    "Hide the current completeions and insert snippets."
     (interactive)
-    (let ((backend company-backend))
-      (company-cancel)
-      (company-begin-backend
-       (if (eq backend 'company-yasnippet)
-           (car company-backends)
-         'company-yasnippet))))
-
-  :config
-  ;; `yasnippet' integration
-  (with-no-warnings
-    (with-eval-after-load 'yasnippet
-      (defun company-backend-with-yas (backend)
-        "Add `yasnippet' to company backend."
-        (if (and (listp backend) (member 'company-yasnippet backend))
-            backend
-          (append (if (consp backend) backend (list backend))
-                  '(:with company-yasnippet))))
-
-      (defun my-company-enbale-yas (&rest _)
-        "Enable `yasnippet' in `company'."
-        (setq company-backends (mapcar #'company-backend-with-yas company-backends)))
-      ;; Enable in current backends
-      (my-company-enbale-yas)
-      ;; Support `company-lsp'
-      (advice-add #'lsp--auto-configure :after #'my-company-enbale-yas)
-
-      (defun my-company-yasnippet-disable-inline (fun command &optional arg &rest _ignore)
-        "Enable yasnippet but disable it inline."
-        (if (eq command 'prefix)
-            (when-let ((prefix (funcall fun 'prefix)))
-              (unless (memq (char-before (- (point) (length prefix)))
-                            '(?. ?> ?\( ?\) ?\[ ?{ ?} ?\" ?' ?`))
-                prefix))
-          (progn
-            (when (and (bound-and-true-p lsp-mode)
-                       arg (not (get-text-property 0 'yas-annotation-patch arg)))
-              (let* ((name (get-text-property 0 'yas-annotation arg))
-                     (snip (format "%s (Snippet)" name))
-                     (len (length arg)))
-                (put-text-property 0 len 'yas-annotation snip arg)
-                (put-text-property 0 len 'yas-annotation-patch t arg)))
-            (funcall fun command arg))))
-      (advice-add #'company-yasnippet :around #'my-company-yasnippet-disable-inline))))
+    (company-cancel)
+    (yas-expand)))
 
 ;; yasnippet
 (leaf yasnippet
   :blackout t
   :hook (after-init-hook . yas-global-mode)
   :bind (:yas-minor-mode-map
-         ("C-t" . my-yasnippet-switch)
-         ("TAB" . nil)
-         ([tab] . nil))
+         ("C-t" . my-yasnippet-switch))
   :init
-  ;; silent yas message
-  ;; (advice-add 'yas--load-snippet-dirs :around #'silent-message-advice)
   (setq yas-triggers-in-field t
         ;; yas-also-indent-empty-lines t
         ;; yas-indent-line 'auto
@@ -125,11 +75,6 @@
 # key: ${3:key}
 # --
 $0`(yas-escape-text yas-selected-text)`")
-  :config
-  ;; (unbind-key "TAB" yas-minor-mode-map)
-  ;; (unbind-key "<tab>" yas-minor-mode-map)
-  ;; no-littering setting changed original
-  (add-to-list 'yas-snippet-dirs 'my-dir-snippet)
 
   ;; mode-switch between lisp-interaction-mode and snippet-mode
   (defun my-yasnippet-switch ()
@@ -137,7 +82,11 @@ $0`(yas-escape-text yas-selected-text)`")
     (if (equal major-mode 'snippet-mode)
         (lisp-interaction-mode)
       (snippet-mode))
-    (evil-insert)))
+    (evil-insert))
+
+  :config
+  ;; no-littering setting changed original
+  (add-to-list 'yas-snippet-dirs 'my-dir-snippet))
 
 (provide 'init-company)
 ;;; init-company.el ends here
