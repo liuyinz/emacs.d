56fae464c3f77eb1960e177fc11147881a3cad07
liuyinz <cheunghsu@outlook.com>
2021-02-25 15:59:44 +0800

    fix : remove console related hooks

--- core/init-frame.el
+++ core/init-frame.el
@@ -7,22 +7,8 @@
 ;; the new frame with (select-frame)
 ;; @https://github.com/purcell/emacs.d/blob/master/lisp/init-frame-hooks.el
 
-(defvar after-make-console-frame-hooks '()
-  "Hooks to run after creating a new TTY frame")
-
 (defvar after-make-window-system-frame-hooks '()
   "Hooks to run after creating a new window-system frame")
-
-(add-hook 'after-make-console-frame-hooks
-          (lambda ()
-            (menu-bar-mode -1)
-            (tool-bar-mode -1)
-            (xterm-mouse-mode 1)
-            (diff-hl-mode -1)
-            ;; enable mouse in cocoa terminal
-            (when (featurep 'ns)
-              (global-set-key (kbd "<mouse-4>") (kbd "<wheel-up>"))
-              (global-set-key (kbd "<mouse-5>") (kbd "<wheel-down>")))))
 
 (defun run-after-make-frame-hooks (frame)
   "Run configured hooks in response to the newly-created FRAME.
