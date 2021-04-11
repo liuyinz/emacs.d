07c4e3a3a96060f2cfe5341c19e857a78e85b9a6
liuyinz <cheunghsu@outlook.com>
2021-04-11 20:19:57 +0800

    fix : shackle rules

--- core/init-window.el
+++ core/init-window.el
@@ -6,8 +6,16 @@
   :hook (after-init-hook . shackle-mode)
   :init
   (setq shackle-default-rule nil)
-  (setq shackle-rules '(("*vterm*" :align 'below :size 0.4)
+  (setq shackle-rules '(
+                        ;; builtin
+                        (("*Warnings*" "*Messages*") :size 0.3 :align 'below)
+                        (("*shell*" "*eshell*" "*ielm*") :popup t :size 0.3 :align 'below)
+                        ;; third-party
+                        ("*vterm*" :align 'below :size 0.4)
                         ("*quickrun*" :select t :size 0.3 :align 'below)
+                        (" *Flycheck checkers*" :select t :size 0.3 :align 'below)
+                        ((flycheck-error-list-mode flymake-diagnostics-buffer-mode)
+                         :select t :size 0.25 :align 'below)
                         )))
 
 (leaf transpose-frame :require t)
