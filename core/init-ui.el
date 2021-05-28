00e1683186bea7beeea809966f9ae4a87c938c53
liuyinz <cheunghsu@outlook.com>
2021-05-29 03:56:23 +0800

    fix : avy color

--- core/init-ui.el
+++ core/init-ui.el
@@ -34,10 +34,17 @@
   ;; modify faces
   (custom-set-faces
    '(cursor ((t (:inherit unspecified :background "#fcce7b" :foreground "#242730"))))
+   '(isearch ((t (:foreground "#242730" :background "#fcce7b" :weight bold))))
+   '(lazy-highlight ((t (:foreground "#242730" :background "#7bc257" :weight bold))))
    ;; selectrum
-   '(selectrum-current-candidate ((t (:foreground "white" :weight bold))))
+   '(selectrum-current-candidate ((t (:foreground "white"))))
    ;; vertico
-   '(vertico-current ((t (:foreground "white" :background "#323642" :extend t :weight bold))))
+   '(vertico-current ((t (:foreground "white" :background "#323642" :extend t))))
+   ;; orderless
+   '(orderless-match-face-0 ((t (:inherit font-lock-string-face :weight bold :inverse-video t))))
+   '(orderless-match-face-1 ((t (:inherit error :weight bold :inverse-video t))))
+   '(orderless-match-face-2 ((t (:inherit font-lock-type-face :weight bold :inverse-video t))))
+   '(orderless-match-face-3 ((t (:inherit font-lock-keyword-face :weight bold :inverse-video t))))
    ;;consult
    '(consult-file ((t (:inherit font-lock-doc-face))))
    ;;marginalia
@@ -49,26 +56,19 @@
    '(diff-hl-change ((t (:background nil))))
    '(diff-hl-insert ((t (:background nil))))
    '(diff-hl-delete ((t (:background nil))))
-   ;; orderless
-   '(orderless-match-face-0 ((t (:inherit font-lock-string-face :weight bold))))
-   '(orderless-match-face-2 ((t (:inherit font-lock-type-face :weight bold))))
-   '(orderless-match-face-1 ((t (:inherit error :weight bold))))
-   '(orderless-match-face-3 ((t (:inherit font-lock-keyword-face :weight bold))))
    ;; markdown-mode
    '(markdown-code-face ((t (:background "#323642" :extend t))))
    ;; indent-guide
    '(indent-guide-face ((t (:inherit font-lock-comment-face))))
    ;; evil
-   '(isearch ((t (:foreground "#242730" :background "#fcce7b" :weight bold))))
-   '(lazy-highlight ((t (:foreground "#242730" :background "#7bc257" :weight bold))))
-   '(evil-ex-substitute-matches ((t (:foreground "#242730" :background "#ff665c" :weight bold))))
-   '(evil-ex-substitute-replacement ((t (:foreground "#242730" :background "#7bc257" ::weight bold))))
-   ;; rg.el
+   '(evil-ex-substitute-matches ((t (:inherit error :weight bold :inverse-video t))))
+   '(evil-ex-substitute-replacement ((t (:inherit font-lock-string-face :weight bold :inverse-video t))))
    ;; avy
-   '(avy-lead-face ((t (:foreground "#7bc257" :background nil))))
-   ;; '(avy-lead-face-0 ((t (:foreground "#9dd282" :background nil))))
-   '(avy-lead-face-0 ((t (:foreground "#bfe1ad" :background nil))))
-   '(avy-lead-face-2 ((t (:foreground "#e1f1d9" :background nil))))
+   '(avy-lead-face ((t (:foreground "#51afef" :background nil))))
+   '(avy-lead-face-0 ((t (:foreground "#99d0f6" :background nil))))
+   '(avy-lead-face-1 ((t (:foreground "#cfe9fb" :background nil))))
+   '(avy-lead-face-2 ((t (:foreground "#f3f9fe" :background nil))))
+   ;; rg.el
    ))
 
 (provide 'init-ui)
