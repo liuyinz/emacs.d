e078c2f3a4ec23e87ac8b78292994b7167818aeb
liuyinz <cheunghsu@outlook.com>
2021-06-08 14:19:56 +0800

    WIP : quickrun shackle bug

--- core/init-window.el
+++ core/init-window.el
@@ -2,6 +2,14 @@
 ;;; Commentary:
 ;;; Code:
 
+(setq display-buffer-alist
+      '(("^\\*quickrun"
+	     (display-buffer-reuse-window
+	      display-buffer-in-side-window)
+	     (reusable-frames . visible)
+	     (side            . bottom)
+	     (window-height   . 0.4))))
+
 (leaf shackle
   :hook (after-init-hook . shackle-mode)
   :init
@@ -9,17 +17,23 @@
   (setq shackle-rules
         '(
           ;; builtin
-          (("*Warnings*" "*Messages*") :size 0.3 :align 'below)
-          (("*shell*" "*eshell*" "*ielm*") :popup t :size 0.3 :align 'below)
+          (("*Warnings*" "*Messages*") :size 0.3)
+          (("*shell*" "*eshell*" "*ielm*") :popup t :size 0.3)
+
           ;; third-party
-          ("*evil-marks*" :align 'below :size 0.4)
+          ;; ("*evil-marks*" :align 'below :size 0.4)
+
           ("*vterm*" :align 'below :size 0.4)
-          ("*quickrun*" :select t :size 0.3 :align 'below)
+          ;; ("*quickrun*" :select t :size 0.4 :align 'below)
           ("*Python*" :select t :size 0.4 :align 'below)
+          ("*nodejs*" :select t :size 0.4 :align 'below)
+
           ("*format-all-errors*" :size 0.3 :align 'below)
+
           (" *Flycheck checkers*" :select t :size 0.3 :align 'below)
           ((flycheck-error-list-mode flymake-diagnostics-buffer-mode)
            :select t :size 0.25 :align 'below)
+
           (" *command-log*" :size 0.4 :align 'left)
           )))
 
