7df8f894636fd385f5939bd63f9fa35e3d96001e
liuyinz <cheunghsu@outlook.com>
2021-06-14 12:22:47 +0800

    feat : add session.el and init-session.el

---
+++ core/init-session.el
@@ -0,0 +1,11 @@
+;;; init-session.el --- session settings -*- lexical-binding: t no-byte-compile: t -*-
+
+;;; Commentary:
+
+;;; Code:
+
+
+
+(provide 'init-session)
+
+;;; init-session.el ends here
