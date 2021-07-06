06043729641c089098cccda710fc700e5eda40e1
liuyinz <cheunghsu@outlook.com>
2021-07-06 18:19:23 +0800

    fix : move ilog to init-tool

--- core/init-edit.el
+++ core/init-edit.el
@@ -57,38 +57,8 @@
 
 ;; undo history
 (leaf undohist
-  :require t
-  :init
-  (setq undohist-ignored-files '("\\.git/COMMIT_EDITMSG$"))
-  :config
-  (undohist-initialize))
-
-(leaf interaction-log
-  :require t
-  :hook (ilog-log-buffer-mode-hook . (lambda ()
-                                       (setq ilog-display-state 'messages)
-                                       (ilog-toggle-view)))
-  :init
-  (setq ilog-log-max nil)
-  :config
-
-  (defun toggle-keylog ()
-    "Toggle keybinds log."
-    (interactive)
-    (unless (bufferp ilog-buffer-name)
-      (interaction-log-mode))
-    (with-current-buffer ilog-buffer-name
-      (let ((win (get-buffer-window (current-buffer))))
-        (if (not (windowp win))
-            (progn
-              (unless interaction-log-mode
-                (interaction-log-mode))
-              (display-buffer (current-buffer)))
-          (if interaction-log-mode
-              (progn
-                (interaction-log-mode -1)
-                (delete-window win))
-            (interaction-log-mode)))))))
+  :hook (after-init-hook . undohist-initialize)
+  :init (setq undohist-ignored-files '("\\.git/COMMIT_EDITMSG$")))
 
 (leaf wgrep
   :hook (grep-mode-hook . wgrep-change-to-wgrep-mode)
--- core/init-tool.el
+++ core/init-tool.el
@@ -49,6 +49,31 @@
               erc-lurker-hide-list '("JOIN" "PART" "QUIT")
               erc-autojoin-channels-alist '(("freenode.net" "#emacs"))))
 
+(leaf interaction-log
+  :require t
+  :hook (ilog-log-buffer-mode-hook . (lambda ()
+                                       (setq ilog-display-state 'messages)
+                                       (ilog-toggle-view)))
+  :init (setq ilog-log-max nil)
+  :config
+  (defun toggle-keylog ()
+    "Toggle keybinds log."
+    (interactive)
+    (unless (bufferp ilog-buffer-name)
+      (interaction-log-mode))
+    (with-current-buffer ilog-buffer-name
+      (let ((win (get-buffer-window (current-buffer))))
+        (if (not (windowp win))
+            (progn
+              (unless interaction-log-mode
+                (interaction-log-mode))
+              (display-buffer (current-buffer)))
+          (if interaction-log-mode
+              (progn
+                (interaction-log-mode -1)
+                (delete-window win))
+            (interaction-log-mode)))))))
+
 (leaf keyfreq
   :hook
   (after-init-hook . keyfreq-mode)
