25c378b827961cc9f18a411c32e9838acce449b7
liuyinz <liuyinz95@gmail.com>
2022-06-15 12:46:09 +0800

    refactor: lsp-setting

--- core/init-lsp.el
+++ core/init-lsp.el
@@ -4,55 +4,63 @@
 
 ;; ------------------------- lsp-mode -----------------------------
 
+;; SEE https://emacs-lsp.github.io/lsp-mode/
 (leaf lsp-mode
-  :hook ((js-mode-hook sh-mode-hook go-mode-hook) . lsp-deferred)
+  :hook ((sh-mode-hook
+          html-mode-hook
+          css-mode-hook
+          js-mode-hook
+          yaml-mode-hook) . lsp-deferred)
   :init
-  (setq read-process-output-max (* 1024 1024)) ;; 1mb
 
-  ;; SEE https://emacs-lsp.github.io/lsp-mode/page/performance/
+  ;; --------------------------- debug -------------------------------
+
+  ;; debugging
+  ;; (setq lsp-log-io t)
+
+  (setq read-process-output-max (* 1024 1024)) ;; 1mb
   (setq lsp-keymap-prefix nil)
   (setq lsp-completion-enable t)
   (setq lsp-completion-provider :none)
-  (setq lsp-idle-delay 0.3)
-  (setq lsp-enable-snippet nil)
-  (setq lsp-enable-links nil)
-  (setq lsp-enable-folding nil)
-  (setq lsp-enable-file-watchers nil)         ;; performance matters
-  (setq lsp-enable-text-document-color nil)   ;; as above
-  (setq lsp-enable-symbol-highlighting nil)   ;; as above
-  (setq lsp-enable-on-type-formatting nil)    ;; as above
-  (setq lsp-enable-indentation nil)
-  (setq lsp-headerline-breadcrumb-enable nil) ;; keep headline clean
-  (setq lsp-modeline-code-actions-enable nil) ;; keep modeline clean
-  (setq lsp-modeline-diagnostics-enable nil)  ;; as above
-  (setq lsp-keep-workspace-alive nil)         ;; auto kill lsp server
-  (setq lsp-eldoc-enable-hover nil)           ;; disable eldoc hover
-  (setq lsp-log-io t)                       ;; debug only
-  (setq lsp-auto-guess-root t)                ;; auto guess root
+  ;; (setq lsp-diagnostics-provider :none)
+
+  ;; ------------------------ performance ----------------------------
 
   ;; REQUIRE export LSP_USE_PLISTS=true
   (setq lsp-use-plists t)
 
-  ;; ;; HACK disbale lsp in md/org-mode
-  ;; (defun ad/disable-lsp-in-md-org (fn)
-  ;;   "Call `FN' when needed"
-  ;;   (unless (string-match "\\.\\(?:md\\|org\\)\\'" (buffer-file-name))
-  ;;     (funcall fn)))
-  ;; (advice-add 'lsp :around #'ad/disable-lsp-in-md-org)
+  ;; performance enhancing
+  (setq lsp-warn-no-matched-clients nil)
+  (setq lsp-keep-workspace-alive nil)
+  (setq lsp-enable-file-watchers nil)
 
-  (leaf lsp-pyright
-    :hook (python-mode-hook . (lambda ()
-                                (require 'lsp-pyright)
-                                (lsp-deferred))))
+  ;; SEE https://emacs-lsp.github.io/lsp-mode/tutorials/how-to-turn-off/
+  (setq lsp-enable-symbol-highlighting nil)
+  (setq lsp-ui-doc-enable nil)
+  (setq lsp-ui-doc-show-with-cursor nil)
+  (setq lsp-ui-doc-show-with-mouse nil)
+  (setq lsp-ui-sideline-enable nil)
+  (setq lsp-ui-sideline-show-code-actions nil)
+  (setq lsp-ui-sideline-show-hover nil)
+  (setq lsp-modeline-code-actions-enable nil)
+  (setq lsp-eldoc-enable-hover nil)
+  (setq lsp-eldoc-render-all nil)
+  (setq lsp-modeline-diagnostics-enable nil)
+  (setq lsp-signature-auto-activate nil)
+  (setq lsp-signature-render-documentation nil)
+  ;; (setq lsp-completion-show-detail nil)
+  (setq lsp-completion-show-kind nil)
+  ;; (setq lsp-headerline-breadcrumb-enable nil)
 
-  ;; REQUIRE deps : ccls
-  (leaf ccls
-    :hook ((c-mode-hook
-            c++-mode-hook) . (lambda () (require 'ccls) (lsp-deferred))))
+  ;; -------------------------- feature ------------------------------
 
-  :defer-config
-  (leaf lsp-modeline :require t)
-  (leaf consult-lsp :require t)
+  (setq lsp-enable-snippet nil)
+  (setq lsp-enable-links nil)
+  (setq lsp-enable-folding nil)
+  (setq lsp-enable-indentation nil)
+  (setq lsp-enable-on-type-formatting nil)
+  (setq lsp-enable-text-document-color nil)
+  (setq lsp-enable-dap-auto-configure nil)
 
   (setq lsp-enable-imenu t
         lsp-imenu-show-container-name nil
@@ -75,43 +83,36 @@
                    (?v "Variables"  font-lock-variable-name-face)))
                 (sh-mode :types
                   ((?f "Functions" font-lock-function-name-face)
-                   (?v "Variables" font-lock-variable-name-face)))
-                )))
+                   (?v "Variables" font-lock-variable-name-face))))))
+
+  ;; -------------------------- server ------------------------------
+
+  ;; emmet-ls setting
+  (defun my/emmet-ls-setup ()
+    "Setup for emmet-ls"
+    (setq-local corfu-auto-delay 0.8
+                corfu-preselect-first t
+                corfu-doc-delay 0.1)
+    (setq-local lsp-completion-show-detail nil))
+  (add-hook 'lsp-emmet-ls-after-open-hook #'my/emmet-ls-setup)
+
+  (leaf lsp-pyright
+    :hook (python-mode-hook . (lambda ()
+                                (require 'lsp-pyright)
+                                (lsp-deferred))))
+
+  ;; ;; REQUIRE deps : ccls
+  ;; (leaf ccls
+  ;;   :hook ((c-mode-hook c++-mode-hook) . (lambda ()
+  ;;                                          (require 'ccls)
+  ;;                                          (lsp-deferred))))
+
+  :defer-config
+
+  (leaf lsp-modeline :require t)
+  (leaf consult-lsp :require t)
+
   )
-
-;; --------------------------- eglot -------------------------------
-
-;; (leaf eglot
-;;   :hook ((python-mode-hook
-;;           js-mode-hook
-;;           bash-mode-hook
-;;           go-mode-hook) . eglot-ensure)
-;;   :defer-config
-;;   (setq eglot-stay-out-of '(flymake company))
-;;   (setq eglot-events-buffer-size 0)
-
-;;   ;; add taliwindcss server
-;;   ;; (add-to-list 'eglot-server-programs '((web-mode :language-id "html") . ("tailwindcss-language-server")))
-
-;;   (with-eval-after-load 'consult-imenu
-;;     (appendq! consult-imenu-config
-;;               '((js-mode :types
-;;                   ((?c "Class"     font-lock-type-face)
-;;                    (?f "Function"  font-lock-function-name-face)
-;;                    (?s "Constant"  font-lock-constant-face)
-;;                    (?m "Method"    font-lock-string-face)
-;;                    (?p "Property"  font-lock-builtin-face)
-;;                    (?v "Variable"  font-lock-variable-name-face)
-;;                    (?e "Fields"     font-lock-warning-face)))
-;;                 (python-mode :types
-;;                   ((?c "Class"     font-lock-type-face)
-;;                    (?f "Function"  font-lock-function-name-face)
-;;                    (?v "Variable"  font-lock-variable-name-face)))
-;;                 (sh-mode :types
-;;                   ((?f "Function"  font-lock-function-name-face)
-;;                    (?v "Variable"  font-lock-variable-name-face)))
-;;                 )))
-;;   )
 
 ;; ------------------------ lsp-bridge ----------------------------
 
