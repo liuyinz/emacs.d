554bfa349781923ad8072d5f44cf3131d547d554
liuyinz <liuyinz95@gmail.com>
2022-10-20 22:59:36 +0800

    feat(acm): setup snippets option

--- core/init-bridge.el
+++ core/init-bridge.el
@@ -11,18 +11,23 @@
 (leaf lsp-bridge
   :hook (after-init-hook . global-lsp-bridge-mode)
   :defer-config
-  ;; acm related
-  (setq acm-enable-doc nil)
-  ;; Setup multi server
-  (appendq! lsp-bridge-multi-lang-server-extension-list
-            '((("css" "less" "scss") . "css_emmet")
-              (("html") . "html_emmet")))
+  (setq acm-enable-quick-access nil
+        acm-enable-tabnine nil
+        acm-backend-yas-candidates-number 10
+        acm-snippet-insert-index 0
+        acm-backend-yas-match-by-trigger-keyword t
+        acm-backend-yas-show-trigger-keyword " [%s]")
+  (setq lsp-bridge-enable-diagnostics nil)
 
   ;; ;; Debug: REQUIRE brew install gdb
   ;; (setq lsp-bridge-enable-log t
   ;;       lsp-bridge-enable-debug t)
 
-  )
+  ;; Setup multi server
+  (appendq! lsp-bridge-multi-lang-server-extension-list
+            '((("css" "less" "scss") . "css_emmet")
+              (("html") . "html_emmet")))
+)
 
 (provide 'init-bridge)
 ;;; init-bridge.el ends here
