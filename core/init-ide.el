08a6892bf312ff2e8d1c981cd45fe78b6971442c
liuyinz <liuyinz95@gmail.com>
2022-12-17 00:13:03 +0800

    fix: use apheleia rather than format-all due to conflict with binky-mode

--- core/init-ide.el
+++ core/init-ide.el
@@ -133,24 +133,41 @@
 (leaf editorconfig
   :hook (shell-mode-hook . editorconfig-mode))
 
-(leaf format-all
+;; ;; ISSUE https://github.com/lassik/emacs-format-all-the-code/issues/220
+;; (leaf format-all
+;;   ;; :require t
+;;   :init
+;;   (setq format-all-debug t)
+;;   (advice-add 'format-all-buffer :before #'format-all-ensure-formatter)
+;;
+;;   (defun my/format ()
+;;     "Formatting current buffer."
+;;     (interactive)
+;;     (cl-case major-mode
+;;       (gitconfig-mode (run-general! indent-region indent-whole-buffer))
+;;       (t (run-general! format-all-region format-all-buffer))))
+;;   :defer-config
+;;   (prependq! format-all-default-formatters
+;;              '(("JSONC" prettier)
+;;                ;; SEE https://google.github.io/styleguide/shellguide.html
+;;                ("Shell" (shfmt "-i" "2" "-bn" "-ci"))))
+;;   )
+
+(leaf apheleia
   :require t
   :init
-  (setq format-all-debug t)
-  (advice-add 'format-all-buffer :before #'format-all-ensure-formatter)
-
+  (setq apheleia-hide-log-buffers t)
   (defun my/format ()
     "Formatting current buffer."
     (interactive)
     (cl-case major-mode
-      (gitconfig-mode (run-general! indent-region indent-whole-buffer))
-      (t (run-general! format-all-region format-all-buffer))))
-  :defer-config
-  (prependq! format-all-default-formatters
-             '(("JSONC" prettier)
-               ;; SEE https://google.github.io/styleguide/shellguide.html
-               ("Shell" (shfmt "-i" "2" "-bn" "-ci"))))
-  )
-
+      ((gitconfig-mode emacs-lisp-mode lisp-interaction-mode)
+        (run-general! indent-region indent-whole-buffer))
+      (t (call-interactively #'apheleia-format-buffer))))
+  :config
+  (alist-set! apheleia-formatters
+              '((shfmt . ("shfmt" "-i" "2" "-bn" "-ci"))))
+  (alist-set! apheleia-mode-alist
+              '((python-mode . (black isort)))))
 (provide 'init-ide)
 ;;; init-ide.el ends here
