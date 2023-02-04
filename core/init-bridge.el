d97e96e4fcff5e58794d87f31cffd46b2c3e83a0
liuyinz <liuyinz95@gmail.com>
2023-02-04 22:13:03 +0800

    refactor: depreciate bridge mode line customization

--- core/init-bridge.el
+++ core/init-bridge.el
@@ -40,14 +40,6 @@
   (setq lsp-bridge-enable-diagnostics nil
         lsp-bridge-disable-backup nil)
 
-  ;; customize mode-line info
-  ;; (advice-add 'lsp-bridge--mode-line-format
-  ;;             :filter-return
-  ;;             (lambda (s) (ignore-errors (s-capitalize (substring s 4 10)))))
-  ;; (setcdr (assoc 'lsp-bridge-mode mode-line-misc-info)
-  ;;         '(lsp-bridge--mode-line-format))
-
-
   (appendq! lsp-bridge-default-mode-hooks
             '(snippet-mode-hook
               git-commit-mode-hook
