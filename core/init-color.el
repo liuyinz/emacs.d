6d4d41f80a3441ce921b15757ddd154651524766
liuyinz <liuyinz95@gmail.com>
2024-01-31 17:38:34 +0800

    disable kurecolor

--- core/init-color.el
+++ core/init-color.el
@@ -8,23 +8,23 @@
 ;;; Code:
 
 ;; TODO write transient menu for color operation
-(leaf kurecolor
-  :init
-  (defun my/kurecolor-variant (hex)
-    (interactive)
-    (let ((lst (number-sequence 0.1 1.0 0.1)))
-      (with-current-buffer (get-buffer-create "*kurecolor-variant")
-        (rainbow-mode 1)
-        (goto-char (point-max))
-        (insert (format "\n\n %s   %s\nsat: %0.2f %s\nbri: %0.2f %s"
-                        hex
-                        (mapconcat (apply-partially #'format "%0.2f") lst "    ")
-                        (kurecolor-hex-get-saturation hex)
-                        (mapconcat (apply-partially #'kurecolor-hex-set-saturation hex) lst " ")
-                        (kurecolor-hex-get-brightness hex)
-                        (mapconcat (apply-partially #'kurecolor-hex-set-brightness hex) lst " ")
-                        ))
-        (display-buffer (current-buffer))))))
+;; (leaf kurecolor
+;;   :init
+;;   (defun my/kurecolor-variant (hex)
+;;     (interactive)
+;;     (let ((lst (number-sequence 0.1 1.0 0.1)))
+;;       (with-current-buffer (get-buffer-create "*kurecolor-variant")
+;;         (rainbow-mode 1)
+;;         (goto-char (point-max))
+;;         (insert (format "\n\n %s   %s\nsat: %0.2f %s\nbri: %0.2f %s"
+;;                         hex
+;;                         (mapconcat (apply-partially #'format "%0.2f") lst "    ")
+;;                         (kurecolor-hex-get-saturation hex)
+;;                         (mapconcat (apply-partially #'kurecolor-hex-set-saturation hex) lst " ")
+;;                         (kurecolor-hex-get-brightness hex)
+;;                         (mapconcat (apply-partially #'kurecolor-hex-set-brightness hex) lst " ")
+;;                         ))
+;;         (display-buffer (current-buffer))))))
 
 (leaf ct)
 
