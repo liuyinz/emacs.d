120c3e84d8c5eb07cc0df16bc6d5b05ffbc80ce3
liuyinz <liuyinz95@outlook.com>
2024-04-26 10:55:00 +0800

    use mise instead of envrc

--- core/init-env.el
+++ core/init-env.el
@@ -7,18 +7,20 @@
 
 ;;; Code:
 
-;; SEE https://github.com/purcell/envrc#usage
-;; add hook as late as possible to call it first.
-(leaf envrc
-  :mode ("\\.envrc\\'" . envrc-file-mode)
-  :hook (after-init-hook . envrc-global-mode)
-  :init
-  ;; write command to add envrc for node
-  ;; for npm/pnpm project
-  ;; create or add path
-  ;; allow envrc
+;; (leaf envrc
+;;   :mode ("\\.envrc\\'" . envrc-file-mode)
+;;   :hook (after-init-hook . envrc-global-mode)
+;;   :init
+;;   ;; write command to add envrc for node
+;;   ;; for npm/pnpm project
+;;   ;; create or add path
+;;   ;; allow envrc
+;;   )
 
-  )
+(leaf mise
+  :hook (after-init-hook . global-mise-mode)
+  :init
+  (setq mise-debug t))
 
 (provide 'init-env)
 ;;; init-env.el ends here
