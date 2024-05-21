6386f1a592a76bdb474f543709dc825c080c17e8
liuyinz <liuyinz95@outlook.com>
2024-05-21 19:55:54 +0800

    add init-bridge-detect.el

---
+++ core/init-bridge-detect.el
@@ -0,0 +1,143 @@
+;;; init-bridge-detect.el --- summary -*- lexical-binding: t no-byte-compile: t -*-
+
+;; Author: liuyinz <liuyinz95@gmail.com>
+;; Created: 2024-05-21 00:21:09
+
+;;; Commentary:
+
+;;; Code:
+
+
+;;; base function
+
+(defun jsreact-p (ext)
+  (or (string= ext "jsx")
+      (memq major-mode '(jtsx-jsx-mode  js-jsx-mode))))
+
+(defun tsreact-p (ext)
+  (or (string= ext "tsx")
+      (memq major-mode '(jtsx-tsx-mode jtsx-typescript-mode tsx-ts-mode))))
+
+(defun markdown-p (ext)
+  (or (member ext '("md" "gfm"))
+      (memq major-mode '(markdown-mode markdown-ts-mode))))
+
+(defun handlebar-p (ext)
+  (or (member ext '("handlebars" "hbs"))
+      (eq major-mode 'handlebars-mode)))
+
+(defun css-like-p (ext)
+  (or (member ext '("css" "less" "scss"))
+      (memq major-mode '(css-mode css-ts-mode less-css-mode scss-mode))))
+
+(defun html-p (ext)
+  (or (memq ext '("htm" "html"))
+      (memq major-mode '(mhtml-mode html-mode html-ts-mode))))
+
+
+;;; single server detect
+
+;; ;; HACK remove sh-mode in default mode list and enable it only when sh-shell is not zsh
+;; (setq lsp-bridge-single-lang-server-mode-list
+;;       (remove (rassoc "bash-language-server" lsp-bridge-single-lang-server-mode-list)
+;;               lsp-bridge-single-lang-server-mode-list))
+
+;; (setq lsp-bridge-get-single-lang-server-by-project #'my/bridge-single-server-detect)
+;; (defun my/bridge-single-server-detect (project-path filepath)
+;;   "Detect right server config for single server."
+;;   (save-excursion
+;;     (let* ((ext (file-name-extension filepath))
+;;            (toml-p (or (string= ext "toml")
+;;                        (memq major-mode '(toml-ts-mode conf-toml-mode))))
+;;            (non-zsh-sh-p
+;;             (or (memq major-mode '(bash-mode bash-ts-mode))
+;;                 (and (eq major-mode 'sh-mode) (not (string= ext "zsh"))))))
+;;       (cond
+;;        (toml-p "toml-language-server")
+;;        ;; (non-zsh-sh-p "bash-language-server")
+;;        ))))
+
+
+;;; multi-server detect
+(appendq! lsp-bridge-multi-lang-server-mode-list
+          '(((jtsx-jsx-mode jtsx-tsx-mode) . "jsreact_tailwindcss")))
+;; (setq lsp-bridge-get-multi-lang-server-by-project 'my/bridge-multi-server-detect)
+;; (defun my/bridge-multi-server-detect (project_path filepath)
+;;   (and (string= (file-name-extension filepath) "html") "html_tailwindcss")
+  ;; (save-excursion
+  ;;   ;; detect for web dev
+  ;;   ;; NOTE project-path return same value as filepath if lsp-bridge cannot detect project
+  ;;   ;; so check it ahead, tailwindcss do not support single file mode
+  ;;   (let ((ext (file-name-extension filepath))
+  ;;         (tailwindcss-p (and (file-directory-p project)
+  ;;                             (directory-files
+  ;;                              project
+  ;;                              'full
+  ;;                              "tailwind\\.config\\.\\(j\\|cj\\|mj\\|t\\)s\\'"))))
+  ;;     (cond
+  ;;      ;; ext/multi
+  ;;      ((and tailwindcss-p (jsreact-p ext)) "jsreact_tailwindcss")
+  ;;      ((and tailwindcss-p (tsreact-p ext)) "tsreact_tailwindcss")
+  ;;      ((and tailwindcss-p (html-p ext)) "html_emmet_tailwindcss")
+  ;;      ((and tailwindcss-p (css-like-p ext)) "css_emmet_tailwindcss")
+  ;;      ;; lib/multi
+  ;;      ((css-like-p ext) "css_emmet")
+  ;;      ((html-p ext) "html_emmet"))))
+  ;; )
+
+
+
+;;; multi-server languageId detect
+
+;; SEE https://github.com/tailwindlabs/tailwindcss-intellisense/blob/master/packages/tailwindcss-language-service/src/util/languages.ts
+;; SEE https://github.com/aca/emmet-ls#readme
+
+(setq lsp-bridge-get-language-id 'my/bridge-get-language-id)
+
+(defun my/bridge-get-language-id (project filepath server ext)
+  (pcase server
+    ;; tailwindcss filetypes:
+    ;; html-kind: 'aspnetcorerazor','astro','astro-markdown','blade','django-html',
+    ;; 'edge', 'ejs', 'erb', 'gohtml', 'GoHTML', 'gohtmltmpl', 'haml', 'handlebars',
+    ;; 'hbs', 'html', 'HTML (Eex)', 'HTML (EEx)','html-eex', 'htmldjango', 'jade',
+    ;; 'leaf', 'liquid', 'markdown', 'mdx','mustache','njk','nunjucks','phoenix-heex',
+    ;; 'php', 'razor', 'slim','surface','twig',
+    ;; css-kind 'css','less', 'postcss', 'sass','scss', 'stylus', 'sugarss','tailwindcss',
+    ;; js-kind: 'javascript','javascriptreact', 'reason', 'rescript',
+    ;; 'typescript','typescriptreact','glimmer-js','glimmer-ts',
+    ("tailwindcss"
+     (pcase ext
+       ((pred jsreact-p) "javascriptreact")
+       ((pred tsreact-p) "typescriptreact")
+       ((pred markdown-p) "markdown")
+       ((pred handlebar-p) "handlebars")
+       ("js" "javascript")
+       ("ts" "typescript")
+       ("res" "rescript")
+       (_ ext)))
+    ;; emmet supports following filetype:
+    ;; 'css', 'eruby', 'html', 'javascript', 'javascriptreact', 'less',
+    ;; 'sass', 'scss', 'svelte', 'pug', 'typescriptreact', "vue"
+    ("emmet-ls"
+     (pcase ext
+       ((pred jsreact-p) "javascriptreact")
+       ((pred tsreact-p) "typescriptreact")
+       ("js" "javascript")
+       ("erb" "eruby")
+       ((or "css" "html" "less" "sass" "scss" "svelte" "pug" "vue") ext)
+       (_ "html")))
+    ;; eslint lsp support
+    ;; 'javascript', 'javascriptreact', 'typescript', 'typescriptreact',
+    ;; 'html', 'vue', 'markdown'
+    ("vscode-eslint-language-server"
+     (pcase ext
+       ((pred jsreact-p) "javascriptreact")
+       ((pred tsreact-p) "typescriptreact")
+       ((pred markdown-p) "markdown")
+       ((pred html-p) "html")
+       ("js" "javascript")
+       ("ts" "typescript")
+       ("vue" "vue")))))
+
+(provide 'init-bridge-detect)
+;;; init-bridge-detect.el ends here
--- core/init-bridge.el
+++ core/init-bridge.el
@@ -18,6 +18,9 @@
 
 ;;; Code:
 
+(leaf lsp-bridge-ref
+  :hook (lsp-bridge-ref-mode-hook . (lambda () (meow-mode -1))))
+
 (leaf lsp-bridge
   :hook (after-init-hook . global-lsp-bridge-mode)
   :init
@@ -34,85 +37,18 @@
     )
   (add-hook 'lsp-bridge-mode-hook #'lsp-bridge-mode-setup -100)
 
-  ;; ;; Debug: REQUIRE brew install gdb
-  ;; (setq lsp-bridge-enable-log t)
-  ;; (setq lsp-bridge-enable-debug t)
-
-  :defer-config
-  ;; Setup server
-  (setq lsp-bridge-user-langserver-dir
-        (expand-file-name "lsp-bridge/single" my/dir-ext))
-  (setq lsp-bridge-user-multiserver-dir
-        (expand-file-name "lsp-bridge/multi" my/dir-ext))
-
-  (setq acm-candidate-match-function 'orderless-flex)
-  (setq lsp-bridge-enable-diagnostics t
-        lsp-bridge-disable-backup nil)
-  (setq lsp-bridge-enable-completion-in-string t)
-  (appendq! lsp-bridge-default-mode-hooks
-            '(snippet-mode-hook
-              git-commit-mode-hook
-              mhtml-mode-hook
-              html-mode-hook
-              js-json-mode-hook
-              vue-ts-mode-hook))
-
   ;; Setup language
   ;; REQUIRE pthon3.11 -m pip install epc orjson sexpdata six paramiko ruff-lsp
   (setq lsp-bridge-python-command "python3.11")
   (setq lsp-bridge-python-multi-lsp-server "pyright_ruff")
 
-  ;; ;; HACK remove sh-mode in default mode list and enable it only when sh-shell is not zsh
-  ;; (setq lsp-bridge-single-lang-server-mode-list
-  ;;       (remove (rassoc "bash-language-server" lsp-bridge-single-lang-server-mode-list)
-  ;;               lsp-bridge-single-lang-server-mode-list))
+  ;; ;; Debug: REQUIRE brew install gdb
+  (setq lsp-bridge-enable-log t)
+  ;; (setq lsp-bridge-enable-debug t)
 
-  ;; (setq lsp-bridge-get-single-lang-server-by-project #'my/bridge-single-server-detect)
-  ;; (defun my/bridge-single-server-detect (project-path filepath)
-  ;;   "Detect right server config for single server."
-  ;;   (save-excursion
-  ;;     (let* ((ext (file-name-extension filepath))
-  ;;            (toml-p (or (string= ext "toml")
-  ;;                        (memq major-mode '(toml-ts-mode conf-toml-mode))))
-  ;;            (non-zsh-sh-p
-  ;;             (or (memq major-mode '(bash-mode bash-ts-mode))
-  ;;                 (and (eq major-mode 'sh-mode) (not (string= ext "zsh"))))))
-  ;;       (cond
-  ;;        (toml-p "toml-language-server")
-  ;;        ;; (non-zsh-sh-p "bash-language-server")
-  ;;        ))))
+  :defer-config
 
-  (setq lsp-bridge-get-multi-lang-server-by-project #'my/bridge-multi-server-detect)
-  (defun my/bridge-multi-server-detect (project-path filepath)
-    "Detect right server config for multi servers.."
-    (save-excursion
-      ;; detect for web dev
-      (let* ((tailwindcss-p (directory-files
-                             project-path
-                             'full
-                             "tailwind\\.config\\.\\(j\\|cj\\|mj\\|t\\)s\\'"))
-             (ext (file-name-extension filepath))
-             (jsx-p (or (string= ext "jsx")
-                        (memq major-mode '(jtsx-jsx-mode  js-jsx-mode))))
-             (tsx-p (or (string= ext "tsx")
-                        (memq major-mode '(jtsx-tsx-mode
-                                           jtsx-typescript-mode
-                                           tsx-ts-mode))))
-             (html-p
-              (or (memq ext '("htm" "html"))
-                  (memq major-mode '(mhtml-mode html-mode html-ts-mode web-mode))))
-             (css-p
-              (or (memq ext '("css"))
-                  (memq major-mode '(css-mode css-ts-mode less-css-mode scss-mode)))))
-        (cond
-         ((and tailwindcss-p jsx-p) "jsreact_tailwindcss")
-         ((and tailwindcss-p tsx-p) "tsreact_tailwindcss")
-         ((and tailwindcss-p html-p) "html_emmet_tailwindcss")
-         ;; ((and tailwindcss-p html-p) "html_tailwindcss")
-         (html-p "html_emmet")
-         ((and tailwindcss-p css-p) "css_emmet_tailwindcss")
-         ;; ((and tailwindcss-p css-p) "css_tailwindcss")
-         (css-p "css_emmet")))))
+  (require 'init-bridge-detect)
 
   (leaf acm
     :init
@@ -147,10 +83,26 @@
             (setq acm-enable-doc nil))
         (setq acm-enable-doc t)
         (acm-doc-try-show)))
-    (advice-add 'acm-doc-toggle :override #'ad/acm-doc-toggle)))
+    (advice-add 'acm-doc-toggle :override #'ad/acm-doc-toggle))
 
-(leaf lsp-bridge-ref
-  :hook (lsp-bridge-ref-mode-hook . (lambda () (meow-mode -1))))
+  ;; Setup server
+  (setq lsp-bridge-user-langserver-dir
+        (expand-file-name "lsp-bridge/single" my/dir-ext))
+  (setq lsp-bridge-user-multiserver-dir
+        (expand-file-name "lsp-bridge/multi" my/dir-ext))
+
+  (setq acm-candidate-match-function 'orderless-flex)
+  (setq lsp-bridge-enable-diagnostics t
+        lsp-bridge-disable-backup nil)
+  (setq lsp-bridge-enable-completion-in-string t)
+  (appendq! lsp-bridge-default-mode-hooks
+            '(snippet-mode-hook
+              git-commit-mode-hook
+              mhtml-mode-hook
+              html-mode-hook
+              js-json-mode-hook
+              vue-ts-mode-hook))
+  )
 
 (provide 'init-bridge)
 ;;; init-bridge.el ends here
