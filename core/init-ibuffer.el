e22e6ea2d90c27daf50f899ccc59fd62c94cd46c
liuyinz <liuyinz95@outlook.com>
2024-05-28 20:37:08 +0800

    refactor ibuffer config

--- core/init-ibuffer.el
+++ core/init-ibuffer.el
@@ -6,24 +6,47 @@
   :hook (ibuffer-mode-hook . ibuffer-mode-setup)
   :bind
   ("C-x C-b" . ibuffer)
+  (:ibuffer-mode-map
+   ("G" . ibuffer-toggle))
   :init
   (setq ibuffer-expert t
         ibuffer-display-summary nil
         ibuffer-show-empty-filter-groups nil
         ibuffer-marked-char ?*)
 
+  (defun ibuffer-toggle ()
+    (interactive)
+    (ibuffer-update 'show-all))
+
+  (setq ibuffer-maybe-show-predicates '(my/ibuffer-not-show-predicate))
+  (defun my/ibuffer-not-show-predicate (buf)
+    (with-current-buffer buf
+      (or (string-suffix-p ".el.gz" (buffer-name))
+          (string-match "^\\*" (buffer-name))
+          (memq major-mode ibuffer-help-buffer-modes)
+          (and (string-match "^ " (buffer-name))
+	           (null buffer-file-name)))))
+
+  (setq ibuffer-fontification-alist
+        '((35 (or (string-suffix-p ".el.gz" (buffer-name))
+                  (string-match "^\\*" (buffer-name))
+                  (memq major-mode ibuffer-help-buffer-modes)
+                  (and (string-match "^ " (buffer-name))
+	                   (null buffer-file-name)))
+              font-lock-doc-face)
+          (100 (derived-mode-p 'dired-mode) dired-directory)))
+
   (with-eval-after-load 'diredfl
     (setq ibuffer-marked-face 'diredfl-flag-mark
           ibuffer-deletion-face 'diredfl-deletion
-          ibuffer-title-face 'diredfl-dir-name
-          ibuffer-filter-group-name-face 'bold))
+          ibuffer-title-face 'diredfl-number
+          ibuffer-filter-group-name-face 'diredfl-symlink))
 
   (setq ibuffer-formats
         '((mark " " (name 20 20 :left :elide)
                 " " modified read-only
                 " " (size-h 7 -1 :right)
                 " " (mode 16 16 :left :elide)
-                " " (vc-status 16 16 :center)
                 " " filename-and-process)
           (mark " " name)))
 
