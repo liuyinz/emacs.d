3fee36052a644b9daa16ab423855e29c39f8f66b
liuyinz <liuyinz95@outlook.com>
2024-06-10 19:38:41 +0800

    rewrite denote-dispatch

--- core/init-write.el
+++ core/init-write.el
@@ -40,42 +40,43 @@
       (rename-buffer new-name :unique)))
   (setq denote-rename-buffer-function #'denote-rename-buffer-with-ext))
 
-(leaf denote-menu)
-(transient-define-prefix denote-dispatch ()
-  "denote commands"
-  ["Create"
-   ("cc" "Default" denote)
-   ("cr" "With region" denote-region)
-   ("ct" "With template" denote-template)
-   ("cn" "With subdirectory" denote-subdirectory)
-   ("cy" "With type" denote-type)
-   ("cd" "With date" denote-date)
-   ("cs" "With signature" denote-signature)]
-  ["Update"
-   ("ur" "Rename file" denote-rename-file)
-   ("uf" "Rename file with front matter" denote-rename-file-using-front-matter)
-   ("uk" "Add keyword" denote-keywords-add)
-   ("ud" "Remove keyword" denote-keywords-remove)
-   ("ua" "Add front matter" denote-add-front-matter)
-   ("uc" "Change type and front matter" denote-change-file-type-and-front-matter)]
-  ["Link"
-   ("lc" "Create link" denote-link)
-   ("la" "Add links" denote-add-links)
-   ("lf" "Find link" denote-find-link)
-   ("lb" "Backlinks" denote-backlinks)
-   ("ls" "Find backlink" denote-find-backlink)]
-  ["Silo"
-   ("sc" "Create" denote-silo-extras-create-note)
-   ("so" "Open or create" denote-silo-extras-open-or-create)
-   ("sr" "Run command" denote-silo-extras-select-silo-then-command)]
-  ["Menu"
-   ("ml" "List notes" denote-menu-list-notes)
-   ("mf" "Filter notes" denote-menu-filter)
-   ("mk" "Filter notes by keyword" denote-menu-filter-by-keyword)
-   ("mK" "Exclude notes by keyword" denote-menu-filter-out-keyword)
-   ("mc" "Clear filters" denote-menu-clear-filters)
-   ("me" "Export to dired" denote-menu-export-to-dired)]
-  )
+(leaf denote-menu
+  :defer-config
+  (transient-define-prefix denote-dispatch ()
+    "denote commands"
+    ["Create"
+     ("cc" "Default" denote)
+     ("cr" "With region" denote-region)
+     ("ct" "With template" denote-template)
+     ("cn" "With subdirectory" denote-subdirectory)
+     ("cy" "With type" denote-type)
+     ("cd" "With date" denote-date)
+     ("cs" "With signature" denote-signature)]
+    ["Update"
+     ("ur" "Rename file" denote-rename-file)
+     ("uf" "Rename file with front matter" denote-rename-file-using-front-matter)
+     ("uk" "Add keyword" denote-keywords-add)
+     ("ud" "Remove keyword" denote-keywords-remove)
+     ("ua" "Add front matter" denote-add-front-matter)
+     ("uc" "Change type and front matter" denote-change-file-type-and-front-matter)]
+    ["Link"
+     ("lc" "Create link" denote-link)
+     ("la" "Add links" denote-add-links)
+     ("lf" "Find link" denote-find-link)
+     ("lb" "Backlinks" denote-backlinks)
+     ("ls" "Find backlink" denote-find-backlink)]
+    ["Silo"
+     ("sc" "Create" denote-silo-extras-create-note)
+     ("so" "Open or create" denote-silo-extras-open-or-create)
+     ("sr" "Run command" denote-silo-extras-select-silo-then-command)]
+    ["Menu"
+     ("ml" "List notes" denote-menu-list-notes)
+     ("mf" "Filter notes" denote-menu-filter)
+     ("mk" "Filter notes by keyword" denote-menu-filter-by-keyword)
+     ("mK" "Exclude notes by keyword" denote-menu-filter-out-keyword)
+     ("mc" "Clear filters" denote-menu-clear-filters)
+     ("me" "Export to dired" denote-menu-export-to-dired)]
+    ))
 
 (provide 'init-write)
 ;;; init-write.el ends here
