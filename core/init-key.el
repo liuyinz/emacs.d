fa0e02b09606b33c9ae2c77798b112c02ab0bda3
liuyinz <liuyinz95@outlook.com>
2024-11-30 03:55:32 +0800

    more general keyboard-quit

--- core/init-key.el
+++ core/init-key.el
@@ -13,7 +13,31 @@
 
 ;;; Code:
 
-(dolist (pair '(("M-j" . scroll-other-window)
+(defun my/keyboard-quit ()
+  "A general `keyboard-quit'.
+The generic `keyboard-quit' does not do the expected thing when
+the minibuffer is open.  Whereas we want it to close the
+minibuffer, even without explicitly focusing it.
+
+The DWIM behaviour of this command is as follows:
+
+- When the region is active, disable it.
+- When a minibuffer is open, but not focused, close the minibuffer.
+- When the Completions buffer is selected, close it.
+- In every other case use the regular `keyboard-quit'."
+  (interactive)
+  (cond
+   ((region-active-p)
+    (keyboard-quit))
+   ((derived-mode-p 'completion-list-mode)
+    (delete-completion-window))
+   ((> (minibuffer-depth) 0)
+    (abort-recursive-edit))
+   (t
+    (keyboard-quit))))
+
+(dolist (pair '(("C-g" . my/keyboard-quit)
+                ("M-j" . scroll-other-window)
                 ("M-k" . scroll-other-window-down)
                 ("M-o" . toggle-one-window)
                 ;; vscode style
